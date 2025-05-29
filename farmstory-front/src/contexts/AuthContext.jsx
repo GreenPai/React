2794ac578fcd5978ed8fc6b1105c01e0c269b3a9
GreenPai <clsrntkdgh@gmail.com>
2025-05-29 11:45:55 +0900

    게시판

---
+++ farmstory-front/src/contexts/AuthContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
+const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    const username = Cookies.get("username");
+    // 쿠키 값이 존재하면 Context username 초기화
+    if (username) {
+      setUsername(username);
+    }
+  }, []);
+
+  const login = (username) => {
+    // Context username 초기화
+    setUsername(username);
+
+    // 쿠키 저장
+    Cookies.set("username", username);
+  };
+
+  const logout = () => {
+    setUsername(null);
+    Cookies.remove("username");
+  };
+
+  return (
+    <AuthContext.Provider value={{ username, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
